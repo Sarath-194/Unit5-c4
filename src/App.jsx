138ab5f41c467c217efa24a673edb74a6dbee9e9
sarath <93651605+Sarath-194@users.noreply.github.com>
2022-02-21 17:06:30 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Home } from './components/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Home Page</h2>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
